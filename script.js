ff2562164f00c895af0d801bda35fc6bbf6a4963
munthernadeen-cloud <munthernadeen@gmail.com>
2025-10-18 02:36:53 +0300

    Update script.js

--- script.js
+++ script.js
@@ -2,3 +2,6 @@ window.addEventListener("load", () => {
   document.body.style.opacity = "1";
   document.body.style.transition = "opacity 1s ease";
 });
+<button id="hello-btn">Say Hi 👋</button>
+<p id="message"></p>
+
